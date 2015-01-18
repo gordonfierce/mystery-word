1bde109235e8f03370778d95efabe8e84687ec66
Gordon Fierce <gordon.fierce@gmail.com>
2015-01-17 19:11:08 -0500

    Finished basic game and tests.

---
+++ mysteryword.py
@@ -0,0 +1,138 @@
+import random
+
+
+def word_getter(remove_proper_nouns=True):
+    the_word = ""
+    word_list = get_list_of_all_possible_words()
+    if remove_proper_nouns:
+        word_list = [word for word in word_list if not word[0].isupper()]
+    the_word = random.choice(word_list)
+    return the_word
+
+
+def get_list_of_all_possible_words():
+    the_word_list = []
+
+    with open("/usr/share/dict/words") as word_file:
+        the_word_string = word_file.read()
+
+    the_word_list = the_word_string.split()
+    return the_word_list
+
+
+def word_display(guessed_string, secret_word):
+    display_string = ""
+    for letter in secret_word:
+        if letter in guessed_string:
+            display_string += letter.upper()
+        else:
+            display_string += ("_")
+    print(display_string)
+    return display_string
+
+
+def user_input_is_correct(input_str):
+    if len(input_str) > 1:
+        return False
+    elif len(input_str) == 0:
+        return False
+    elif not input_str.isalpha():
+        return False
+    else:
+        return True
+
+
+def guess_is_new(guessed_string, user_input):
+    if user_input in guessed_string:
+        return False
+    else:
+        return True
+
+
+def guess_appender(guessed_string, user_input):
+    guess_string = guessed_string + user_input
+    return guess_string
+
+
+def not_incorrect_or_repeat_guess(guessed_string, user_input):
+    if not guess_is_new(guessed_string, user_input):
+        print("You already guessed that!")
+        return False
+    elif not user_input_is_correct(user_input):
+        print("Nope, give me a single letter")
+        return False
+    else:
+        return True
+
+
+def guess_display(guessed_string, secret_word, max_guesses):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    guesses_list = []
+    guesses_string = ""
+    for letter in alphabet:
+        if not letter in guessed_string:
+            guesses_list.append(letter.upper())
+        else:
+            pass
+    guesses_string = "".join(sorted(guesses_list))
+    guesses_remain = max_guesses - spent_guesses(guessed_string, secret_word)
+    print_string = "Letters remaining: {} Guesses remaining: {}."
+    print(print_string.format(guesses_string, guesses_remain))
+    return print_string.format(guesses_string, guesses_remain)
+
+
+def you_win(guessed_string, secret_word):
+    for letter in secret_word:
+        if letter not in guessed_string:
+            return False
+    return True
+
+
+def you_lose(guessed_string, secret_word, max_guesses):
+    if spent_guesses(guessed_string, secret_word) >= max_guesses:
+        return True
+    else:
+        return False
+
+
+def spent_guesses(guessed_string, secret_word):
+    """Given the word and the string of guesses, return how many guesses
+    have been spent."""
+    right_guesses = 0
+    for letter in guessed_string:
+        if letter in secret_word:
+            right_guesses += 1
+    total_guesses = len(guessed_string)
+    spent_guesses = total_guesses - right_guesses
+    return spent_guesses
+
+
+def check_win_lose(guessed_string, secret_word, max_guesses):
+    if you_win(guessed_string, secret_word):
+        print("You win!")
+        print("Your word was: {}".format(secret_word.upper()))
+        return "You win!"
+
+    elif you_lose(guessed_string, secret_word, max_guesses):
+        print("You lose!")
+        print("Your word was: {}".format(secret_word.upper()))
+        return "You lose!"
+    else:
+        pass
+
+
+if __name__ == '__main__':
+    print("Welcome to Mystery Word!")
+    secret_word = word_getter()
+    print("Your word is {} letters long!".format(len(secret_word)))
+    max_guesses = 8  # set to 8, but use 26 to test
+    guessed_string = ""
+    while True:
+        word_display(guessed_string, secret_word)
+        guess_display(guessed_string, secret_word, max_guesses)
+        curr_guess = input("Guess a letter: ")
+        if not_incorrect_or_repeat_guess(guessed_string, curr_guess):
+            guessed_string = guess_appender(guessed_string, curr_guess)
+        win_state = check_win_lose(guessed_string, secret_word, max_guesses)
+        if win_state is not None:
+            break
---
+++ test_mysteryword.py
@@ -0,0 +1,87 @@
+import mysteryword as mw
+
+def test_word_getter_got_something():
+    word = mw.word_getter()
+    assert word is not None
+
+def test_word_getter_got_string():
+    word = mw.word_getter()
+    assert type(word) == str
+
+def test_get_list_got_list():
+    a_list = mw.get_list_of_all_possible_words()
+    assert type(a_list) == list
+
+def test_strings_are_in_list():
+    a_word = mw.get_list_of_all_possible_words()[0]
+    assert type(a_word) == str
+
+def test_some_words_are_in_all_words():
+    a_list = mw.get_list_of_all_possible_words()
+    some_words = ["waterfall", "rain", "aardvark"]
+    for word in some_words:
+        assert word in a_list
+
+def test_user_input_is_alpha():
+    user_input = mw.user_input_is_correct("#")
+    assert not user_input
+    user_input = mw.user_input_is_correct("t")
+    assert user_input
+
+def test_user_input_is_no_longer_than_one():
+    user_input = mw.user_input_is_correct("o")
+    assert user_input
+    user_input = mw.user_input_is_correct("nots")
+    assert not user_input
+
+def test_user_input_isnt_empty():
+    user_input = mw.user_input_is_correct("")
+    assert not user_input
+
+def test_word_display():
+    display_string = mw.word_display("bcmdq","bombard")
+    assert display_string == "B_MB__D"
+    display_string = mw.word_display("lew","waterfall")
+    assert display_string == "W__E___LL"
+
+def test_guess_display():
+    displaystring = mw.guess_display("bomar", "bombard", 8)
+    assert displaystring == "Letters remaining: CDEFGHIJKLNPQSTUVWXYZ Guesses remaining: 8."
+    displaystring = mw.guess_display("b", "bombard", 8)
+    assert displaystring == "Letters remaining: ACDEFGHIJKLMNOPQRSTUVWXYZ Guesses remaining: 8."
+    displaystring = mw.guess_display("bomarq", "bombard", 8)
+    assert displaystring == "Letters remaining: CDEFGHIJKLNPSTUVWXYZ Guesses remaining: 7."
+
+def test_guess_is_new():
+    assert not mw.guess_is_new("axo", "a")
+    assert mw.guess_is_new("axot", "q")
+
+def test_guess_appender():
+    new_guess_string = mw.guess_appender("ax", "o")
+    assert new_guess_string == ("axo")
+
+def test_not_incorrect_or_repeat_guess():
+    result = mw.not_incorrect_or_repeat_guess("bomar", "b")
+    assert not result
+    result = mw.not_incorrect_or_repeat_guess("bomar", "!")
+    assert not result
+
+def test_you_win():
+    assert mw.you_win("bomard","bombard")
+    assert not mw.you_win("bomar", "bombard")
+
+def test_you_lose():
+    assert mw.you_lose("qbcef", "toward", 5)
+    assert not mw.you_lose("abcef", "toward", 5)
+
+def test_spent_guesses():
+    spent_num = mw.spent_guesses("bcmdq","bombard")
+    assert spent_num == 2
+
+def test_check_win_lose():
+    game_eval = mw.check_win_lose("qbcef", "toward", 5)
+    assert game_eval == "You lose!"
+    game_eval = mw.check_win_lose("bomard","bombard", 5)
+    assert game_eval == "You win!"
+    game_eval = mw.check_win_lose("abcef", "toward", 5)
+    assert game_eval == None
